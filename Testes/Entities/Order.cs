0facde9bc76a06450dd6d7c44af3e614f507a0f0
Matheus Azevedo <matheus2002azevedo@icloud.com>
2024-08-14 18:44:37 -0300

    starting to understand enumerables in C#

---
+++ Testes/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using Testes.Entities.Enums;
+
+namespace Testes.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ Testes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Testes.Entities;
+using Testes.Entities.Enums;
+
+namespace Testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+        }
+    }
+}
